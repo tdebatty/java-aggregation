53855e1efada27324bc3a4020f1e9eb477bb3a24
Thibault Debatty <thibault.debatty@gmail.com>
2017-01-06 11:30:57 +0100

    Code clean : lines is now 0 based

--- src/main/java/info/debatty/java/aggregation/Lines.java
+++ src/main/java/info/debatty/java/aggregation/Lines.java
@@ -25,17 +25,16 @@ package info.debatty.java.aggregation;
 
 class Lines {
 
-    StraightLine[] lines;
+    private StraightLine[] lines;
 
     static boolean McAllister = false;
     // false = Modificacio meva C & O
     // true  = Alternativa de McAllister i Roulier segons Iqbal
 
-    static double infinit = Double.MAX_VALUE;
+    private static final double INFINITY = Double.MAX_VALUE;
 
-    Lines(Point[] points) {
+    Lines(final Point[] points) {
 
-        // points starts at 0 and lines starts at 1 :(
         lines = new StraightLine[points.length];
 
         for (int i = 0; i < points.length; i++) {
@@ -43,54 +42,57 @@ class Lines {
         }
 
 
-        double[] s = new double[points.length + 1];
+        // will store the angular coeficient between successive points
+        double[] coefs = new double[points.length + 1];
         double[] m = new double[points.length + 1];
 
         int i = 0;
-        int N = points.length + 1 - 1; //N es la darrera posicio de d, i d es num_values+1
+        int N = points.length;
 
         for (i = 2; i <= N; i++) {
-            s[i] = Point.computeCoef(points[i - 2], points[i - 1]);
+            coefs[i] = Point.computeCoef(points[i - 2], points[i - 1]);
         }
 
         for (i = 2; i <= N - 1; i++) {
-            m[i] = Point.calculaMi(s[i], s[i + 1],
+            m[i] = Point.calculaMi(
+                    coefs[i],
+                    coefs[i + 1],
                     points[i - 1],
                     points[i - 2],
                     points[i]);
         }
 
         if (McAllister) {
-            if ((s[2] * (2 * s[2] - m[2])) > 0.0) {
-                m[1] = 2 * s[2] - m[2];
+            if ((coefs[2] * (2 * coefs[2] - m[2])) > 0.0) {
+                m[1] = 2 * coefs[2] - m[2];
             } else {
                 m[1] = 0.0;
             }
 
         } else {
-            if ((m[2] == 0.0) && (s[2] == 0.0)) {
+            if ((m[2] == 0.0) && (coefs[2] == 0.0)) {
                 m[1] = 0.0;
             } else if (m[2] == 0.0) {
-                m[1] = infinit;
+                m[1] = INFINITY;
             } else {
-                m[1] = s[2] * s[2] / m[2];
+                m[1] = coefs[2] * coefs[2] / m[2];
             }
         }
 
         if (McAllister) {
-            if ((s[N] * (2 * s[N] - m[N - 1])) > 0.0) {
-                m[N] = 2 * s[N] - m[N - 1];
+            if ((coefs[N] * (2 * coefs[N] - m[N - 1])) > 0.0) {
+                m[N] = 2 * coefs[N] - m[N - 1];
             } else {
                 m[N] = 0.0;
             }
 
         } else {
-            if ((m[N - 1] == 0.0) && (s[N] == 0.0)) {
+            if ((m[N - 1] == 0.0) && (coefs[N] == 0.0)) {
                 m[N] = 0.0;
             } else if (m[N - 1] == 0.0) {
-                m[N] = infinit;
+                m[N] = INFINITY;
             } else {
-                m[N] = s[N] * s[N] / m[N - 1];
+                m[N] = coefs[N] * coefs[N] / m[N - 1];
             }
         }
 
@@ -104,7 +106,12 @@ class Lines {
         McAllister = true;
     }
 
-    public StraightLine get(int position) {
+    /**
+     * Get the line at given position.
+     * @param position
+     * @return
+     */
+    public StraightLine get(final int position) {
         return lines[position];
     }
 }
--- src/main/java/info/debatty/java/aggregation/Point.java
+++ src/main/java/info/debatty/java/aggregation/Point.java
@@ -21,6 +21,7 @@ class Point {
 
     /**
      * Copy constructor.
+     *
      * @param other
      */
     Point(Point other) {
@@ -28,10 +29,9 @@ class Point {
         this.y = other.y;
     }
 
-
     /**
-     * Evaluate order 2 bernstein polynomial.
-     * β0.(1-x)² + β1.2x.(1-x) + β2.x²
+     * Evaluate order 2 bernstein polynomial. β0.(1-x)² + β1.2x.(1-x) + β2.x²
+     *
      * @param p0
      * @param p1
      * @param p2
@@ -50,9 +50,9 @@ class Point {
             return b0;
         }
 
-        double y =
-                  b0 * (p2x - x) * (p2x - x)
-                + b1 * 2.0  * (x - p0x) * (p2x - x)
+        double y
+                = b0 * (p2x - x) * (p2x - x)
+                + b1 * 2.0 * (x - p0x) * (p2x - x)
                 + b2 * (x - p0x) * (x - p0x);
 
         y = y / ((p2x - p0x) * (p2x - p0x));
@@ -60,9 +60,9 @@ class Point {
         return (y);
     }
 
-
     /**
      * Compute the angular coefficient between points p1 and p2: Δy/Δx.
+     *
      * @param p2
      * @param p1
      * @return
@@ -71,30 +71,37 @@ class Point {
         return (p2.y - p1.y) / (p2.x - p1.x);
     }
 
-    public static double calculaMi(double si, double siP1, Point di,
-            Point di_1, Point diP1) {
+    /**
+     *
+     * @param coef0 angular coef between p0 and p1
+     * @param coef1 angular coef between p1 and p2
+     * @param p1
+     * @param p0
+     * @param p2
+     * @return
+     */
+    public static double calculaMi(
+            final double coef0,
+            final double coef1,
+            final Point p1,
+            final Point p0,
+            final Point p2) {
 
-        double bx, cx, resu;
+        assert (coef0 * coef1) >= 0.0;
 
-        if ((si * siP1) < 0.0) {
-            resu = 0.0;
-            if (si * siP1 != 0.0) {
-                System.out.println("QUANTIFIER: Not eq!! ha de ser monoton:");
-                System.out.println("(si,siP1)=(" + si + "," + siP1 + ")");
-            }
-        } else if (Math.abs(si) > Math.abs(siP1)) {
-            bx = (diP1.y - di.y) / si + di.x;
-            cx = (bx + diP1.x) / 2.0;
-            resu = (diP1.y - di.y) / (cx - di.x);
-        } else if (Math.abs(si) < Math.abs(siP1)) {
-            bx = (di.x - (di.y - di_1.y) / siP1);
-            cx = (bx + di_1.x) / 2.0;
-            resu = (di.y - di_1.y) / (di.x - cx);
-        } else {
-            resu = si;
+        if (Math.abs(coef0) > Math.abs(coef1)) {
+            double bx = (p2.y - p1.y) / coef0 + p1.x;
+            double cx = (bx + p2.x) / 2.0;
+            return (p2.y - p1.y) / (cx - p1.x);
         }
-        return (resu);
-    }  /* ecalculaMi */
 
+        if (Math.abs(coef0) < Math.abs(coef1)) {
+            double bx = (p1.x - (p1.y - p0.y) / coef1);
+            double cx = (bx + p0.x) / 2.0;
+            return (p1.y - p0.y) / (p1.x - cx);
+        }
+
+        return coef0;
+    }
 
 }
